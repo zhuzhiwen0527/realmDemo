ec7eefc90a567b3e78d1223c09aa454aea62a6b0
zzw <602129484@qq.com>
2016-06-07 17:59:16 +0800

    创建项目

---
+++ realm/realm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  realm
+//
+//  Created by zzw on 16/6/7.
+//  Copyright © 2016年 zzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ realm/realmTests/realmTests.m
@@ -0,0 +1,39 @@
+//
+//  realmTests.m
+//  realmTests
+//
+//  Created by zzw on 16/6/7.
+//  Copyright © 2016年 zzw. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface realmTests : XCTestCase
+
+@end
+
+@implementation realmTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
